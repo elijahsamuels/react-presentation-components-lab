7be4cc2b6666430a33e90db8a5dcb85599bcb6db
Elijah Samuels <elijahsamuels@gmail.com>
2021-03-24 13:03:14 -0600

    commit tree 1bd2faac622123ff388494e18f7536cfff4535c3

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,20 @@
-// Code SimpleComponent Here
+import React, {Component} from 'react';
+
+export default class SimpleComponent extends Component {
+	
+	// Initial state here...
+	state = {
+		mood: "happy"
+	};
+	
+	handleClick = () => {
+		const newMood = this.state.mood !== 'sad' ? 'sad' : 'happy';
+		this.setState({ mood: newMood });
+		}
+	
+	render() {
+		return <div onClick={this.handleClick}>
+			{this.state.mood}
+			</div>;
+		}
+  }
